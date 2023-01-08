e0a8c8945c9d1cf3aa2eb5cff2711f6beaa7bc35
tigp <hnztkfc@gmail.com>
2023-01-08 17:02:35 +0300

    added styles for app and moved routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './components/App';
-import ErrorPage from './components/ErrorPage';
-import Test from './components/Test';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: 'test',
-    element: <Test />,
-  },
-]);
+import App from './App';
+import './styles/index.sass';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 ); // npx eslint . --ext .js,.jsx,.ts,.tsx
